b4e51d6dec4ba937f0a0582ee1bae5411dccaef2
developer <lmurray@trinistorm.org>
2012-08-10 04:58:58 -0400

    Affected by bad classloader Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ proof/util/Mock.php
@@ -0,0 +1,28 @@
+<?php
+namespace proof\util;
+
+/**
+ * timestamp Aug 9, 2012 8:13:58 PM
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package
+ *
+ *
+ */
+class Mock
+{
+
+    /**
+     *
+     * @var
+     * @access
+     */
+
+    public function getMock()
+    {
+        return new Mock;
+    }
+
+}
