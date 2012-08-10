614c046bafbd450e6dd44c4289802f43d7cc6336
developer <lmurray@trinistorm.org>
2012-08-10 05:09:43 -0400

    Updated comment Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ proof/util/Vector.php
@@ -0,0 +1,18 @@
+<?php
+namespace proof\util;
+/**
+ * timestamp Jul 22, 2012 7:10:25 AM
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package proof\util
+ *
+ * Container for  storing items in sequence
+ *
+ */
+class Vector extends AbstractListing
+{
+
+
+}
