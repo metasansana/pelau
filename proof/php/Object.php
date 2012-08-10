1b493ac19d7de5ee45a1cbc60fdaabb118f6abad
developer <lmurray@trinistorm.org>
2012-08-10 19:25:25 -0400

    Added the object class. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ proof/php/Object.php
@@ -0,0 +1,20 @@
+<?php
+namespace proof\php;
+
+/**
+ * timestamp Aug 10, 2012 6:48:47 PM
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package proof\php
+ *
+ *  Class representing a generic object.
+ *
+ */
+class Object
+{
+
+    
+
+}
