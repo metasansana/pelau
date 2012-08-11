79db3352b49968c5880bc4ff40fe1232b0295681
developer <lmurray@trinistorm.org>
2012-08-11 11:29:03 -0400

    thrown on invalid regexs Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ proof/regex/RegexException.php
@@ -0,0 +1,19 @@
+<?php
+namespace proof\regex;
+
+/**
+ * timestamp Aug 11, 2012 10:41:57 AM
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package proof\regex
+ *
+ *  Exception thrown when a regex related error occurs.
+ *
+ */
+class RegexException extends \Exception
+{
+
+
+}
