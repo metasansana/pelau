88581fa2cbb36f1fc6c930eb1e19bb7628e1226f
developer <lmurray@trinistorm.org>
2012-08-17 23:37:18 -0400

    Use a single state listener. Signed-off-by:developer <lmurray@trinistorm.org>

--- proof/sql/Statement.php
+++ proof/sql/Statement.php
@@ -2,7 +2,6 @@
 
 namespace proof\sql;
 
-
 /**
  * timestamp Aug 4, 2012 8:35:08 AM
  *
@@ -15,71 +14,46 @@ namespace proof\sql;
  *  sql statement which can be INSERT,DELETE, SELECT, UPDATE or vendor specific commands.
  *
  */
-use proof\util\ArrayList;
+use proof\php\String;
 
-abstract class Statement
+abstract class Statement implements Fetchable, Pushable
 {
 
     /**
-     * The string version of the sql statement to be executed.
-     * @var string $stmt
-     * @access protected
-     */
-    protected $stmt;
-
-    /**
-     * The PDOProvider to use for execution.
-     * @var proof\sql\PDOProvider $provider
-     * @access protected
-     */
-    protected $provider;
-
-    /**
-     * The PDO object supplied by the provider. @todo This may completely replace the provider
-     * @var \PDO $pdo
-     * @access protected
-     */
-    protected $pdo;
-
-    /**
-     * Handler for the status of SQL commands.
-     * @var proof\util\ArrayList;
+     * Object listening for state changes.
+     * @var proof\sql\SQLStateListener
      * @access protected.
      */
-    protected $handlers;
+    protected $listener;
 
-    public function __construct(PDOProvider $provider, $stmt = NULL)
+   /**
+     * Alerts the SQLStateListener (if any) of a change in SQL state.
+     * @param \proof\sql\SQLState $state
+     * @return boolean
+     */
+    protected function changeState(SQLState $state)
     {
 
-        $this->provider = $provider;
-        $this->pdo = $provider->getPDO();
-        $this->stmt = $stmt;
-        $this->handlers = new ArrayList;
-
-    }
-
-    public function addStatusListener(SQLStatusListener $h)
-    {
-        $this->handlers->add($h);
-        return $this;
-    }
-
-    protected function raiseFailureFlag($info)
-    {
-        foreach($this->handlers as $h)
+        if ($this->listener)
         {
-            $h->onChange($this->provider, new SQLStatus($info));
+            $this->listener->onChange($state);
+            return true;
         }
+        else
+        {
+            return false;
+        }
+
     }
 
     /**
-     * Sets the executable statement
-     * @param string $cmd
+     * Attaches an SQLStateListener
+     * @param \proof\sql\SQLStateListener $l
+     * @return \proof\sql\Statement
      */
-    public function setCommand($cmd)
+    public function attachStateListener(SQLStateListener $l)
     {
-        $this->stmt = $cmd;
-
+        $this->listener = $l;
         return $this;
 
     }
