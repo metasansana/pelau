8945a168afabea1d8b6ef94503447518640947c0
developer <lmurray@trinistorm.org>
2012-09-08 09:56:24 -0400

    Removed old classes, now using an event structure. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ net/http/HttpEvent.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace;
+
+/**
+ * timestamp Sep 8, 2012 9:55:47 AM
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package
+ *
+ *
+ */
+class HttpEvent
+{
+
+    /**
+     *
+     * @var
+     * @access
+     */
+
+}
