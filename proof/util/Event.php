4e24bed712e1023cc094883036ac14dea781a940
developer <lmurray@trinistorm.org>
2012-09-08 09:32:00 -0400

    Merged duplicate Event class. Signed-off-by:developer <lmurray@trinistorm.org>

--- proof/util/Event.php
+++ proof/util/Event.php
@@ -1,22 +1,58 @@
 <?php
 namespace proof\util;
 /**
- * timestamp Jul 28, 2012 11:26:37 AM
+ * timestamp Sep 7, 2012 11:30:48 PM
  *
  *
  * @author Lasana Murray  <dev@trinistorm.org>
  * @copyright 2012 Lasana Murray
  * @package proof\util
  *
- *  Events are methods for passing messages between classes that do not directly know about each
- *  other.
+ *  Parent class of all events. Event classes provide a way to route messages internally between the
+ *  classes in an application
  *
  */
+use proof\php\Object;
+use proof\php\String;
 
-interface Event
+class Event extends Object
 {
 
-    public function getSource();
-    
+    /**
+     * The source object of this event
+     * @var proof\php\Object $source
+     * @access protected
+     */
+    protected $source;
+
+    /**
+     * Constructs a new Event object.
+     * @param Object $source
+     */
+    public function __construct(Object $source)
+    {
+
+        $this->source = $source;
+
+
+    }
+
+    /**
+     * Returns the object that is the source of this event.
+     * @return proof\php\Object
+     */
+    public function getSource()
+    {
+        return $this->source;
+    }
+
+
+    public function getMessage()
+    {
+        $type = $this->getClassName();
+        return new String("An event of type: $type has occured.");
+    }
+
+
 
 }
