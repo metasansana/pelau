8a927c367ffda3d5751fa78a1d7b4dd6da000f9b
developer <lmurray@trinistorm.org>
2012-09-09 20:23:25 -0400

    Consider the last path change. Signed-off-by:developer <lmurray@trinistorm.org>

--- tests/testclassloader.php
+++ tests/testclassloader.php
@@ -7,7 +7,9 @@
 spl_autoload_register(function ($class) {
 
             $path = get_include_path();
-            
+
+            $class = str_replace("proof\\", NULL, $class);
+
             print "Searching for the $class class.. in $path.\n";
 
             $class = str_replace("\\", '/', $class) . ".php";
