6ea4ec0dddb790fac73bfb08786daed2d93dd8fd
developer <lmurray@trinistorm.org>
2012-09-10 20:35:28 -0400

    WindowComponent Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ proof/app/WComponent.php
@@ -0,0 +1,22 @@
+<?php
+namespace proof\app;
+/**
+ * timestamp Sep 10, 2012 4:49:16 AM
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package proof\app
+ *
+ * The Component interface is for classes that store html text.
+ *
+ *
+ */
+interface WComponent
+{
+
+    public function getName();
+
+    public function getContent();
+
+}
