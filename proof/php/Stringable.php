efc5cd08be1959fd30c05ef763684682dda78fd4
developer <lmurray@trinistorm.org>
2012-09-11 05:14:09 -0400

    Magic method interface. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ proof/php/Stringable.php
@@ -0,0 +1,19 @@
+<?php
+namespace proof\php;
+/**
+ * timestamp Sep 11, 2012 5:13:06 AM
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package proof\php
+ *
+ *  Interface for objects that can be converted into a string.
+ *
+ */
+interface Stringable
+{
+
+    public function __toString();
+
+}
