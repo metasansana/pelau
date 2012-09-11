98e13a08b31a55d419c30ae0aa5ebce56d9d7c84
developer <lmurray@trinistorm.org>
2012-09-11 05:22:26 -0400

    Added constructor. Signed-off-by:developer <lmurray@trinistorm.org>

--- proof/app/WebController.php
+++ proof/app/WebController.php
@@ -16,5 +16,18 @@ use proof\net\http\AbstractHttpListener;
 abstract class WebController extends AbstractHttpListener implements Controller
 {
 
+    /**
+     * The view for the web controller
+     * @var proof\app\Window    $view
+     * @access protected
+     */
+    protected $view;
+
+    public function __construct(Window $w)
+    {
+
+        $this->view = $w;
+
+    }
 
 }
