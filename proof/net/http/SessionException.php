cb1da44e1ede89f31ecc366de604d44375b732ac
developer <lmurray@trinistorm.org>
2012-09-23 11:06:58 -0400

    Exception hierarchy. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ proof/net/http/SessionException.php
@@ -0,0 +1,19 @@
+<?php
+namespace proof\net\http;
+/**
+ * timestamp Sep 22, 2012 1:07:41 PM
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package proof\net\http
+ *
+ *  Root class for session related exceptions.
+ *
+ *
+ */
+class SessionException extends HttpException
+{
+
+
+}
