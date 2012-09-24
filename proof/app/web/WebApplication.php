718106d3e2efb036224a23f77958c18adb4974b5
developer <lmurray@trinistorm.org>
2012-09-24 13:10:34 -0400

    Added additional application interfaces. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ proof/app/web/WebApplication.php
@@ -0,0 +1,24 @@
+<?php
+namespace proof\app\web;
+/**
+ * timestamp Sep 24, 2012 11:52:39 AM
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package proof\app\web
+ *
+ * Application interface for database driven web applications.
+ *
+ *
+ */
+use proof\app\DatabaseApplication;
+
+interface  WebApplication extends DatabaseApplication
+{
+
+      public  function getSessionManager();
+
+      public function getCookieManager();
+
+}
