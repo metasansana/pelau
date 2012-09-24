7061ba5bfc39c1b7622b8b51524d3d921220b3b4
developer <lmurray@trinistorm.org>
2012-09-24 14:34:37 -0400

    Added this interface. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ proof/net/http/HttpSource.php
@@ -0,0 +1,17 @@
+<?php
+namespace proof\net\http;
+/**
+ * timestamp Sep 24, 2012 2:33:41 PM
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package proof\net\http
+ *
+ *  Interface for classes that fire HttpEvents.
+ *
+ */
+interface HttpSource
+{
+
+}
