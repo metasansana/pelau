92619f6c8d0a152739d1b6b89442ff65795a5845
metasansana <dev@trinistorm.org>
2012-09-23 22:09:09 -0400

    stuff

---
+++ proof/app/WebApplication.php
@@ -0,0 +1,24 @@
+<?php
+namespace proof\app;
+/**
+ * timestamp Sep 23, 2012 11:23:35 AM
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package proof\app
+ *
+ *  Parent class for the main class of web applicatons.
+ *
+ */
+abstract class WebApplication extends Application
+{
+
+    /**
+     *
+     * @var
+     * @access
+     */
+    protected  $database;
+
+}
