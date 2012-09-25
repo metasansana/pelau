fcb59a0e3a48b3b85afb9044c5263321f1410515
metasansana <dev@trinistorm.org>
2012-09-25 03:50:26 -0400

    Clean up

--- examples/public/index.php
+++ examples/public/index.php
@@ -1,7 +1,18 @@
 <?php
-
+$start  = microtime();
 require_once 'proof.php';
+new proof\util\ArrayList;
+new proof\util\Map;
+new proof\php\String;
+new \proof\php\Object();
 
-phpinfo();
+session_name('bdigsijpfg');
+
+$time = microtime() - $start;
+
+echo "<p>$time</p>";
+
+
+
 
 ?>
