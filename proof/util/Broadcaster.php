41383574049e767a1b33c651dabfc80073e8afb5
developer <lmurray@trinistorm.org>
2012-09-26 05:01:43 -0400

    Broadcaster is a client to the EventBus. Signed-off-by:developer <lmurray@trinistorm.org>

--- proof/util/Broadcaster.php
+++ proof/util/Broadcaster.php
@@ -1,29 +1,20 @@
 <?php
 namespace proof\util;
+
 /**
- * timestamp Sep 8, 2012 3:24:55 PM
+ * timestamp Sep 26, 2012 4:59:21 AM
  *
  *
  * @author Lasana Murray  <dev@trinistorm.org>
  * @copyright 2012 Lasana Murray
  * @package proof\util
  *
- *  Interface for fowarding events.
+ *  Interface for classes that publish events to an EventBus.
  *
  */
 interface Broadcaster
 {
 
-    /**
-     * Adds an EventHandler to the bus.
-     * @param \proof\util\EventHandler $h
-     */
-    public function subscribe(EventHandler $h);
-
-    /**
-     * Puts up an event to be broadcasted.
-     * @param \proof\util\Event $e
-     */
-    public function broadcast(Event $e);
+    public function registerEventBus(EventBus $b);
 
 }
