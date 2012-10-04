e8f5ab81b2cf883e5e3d2f904bfce0cd6f8b6914
developer <lmurray@trinistorm.org>
2012-10-04 05:35:45 -0400

    Added the transaction interface. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ proof/sql/Transaction.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace proof\sql;
+
+
+/**
+ * timestamp Oct 4, 2012 5:33:38 AM
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package proof\sql
+ *
+ * Interface for using SQL transaction (Auto commit off).
+ *
+ */
+interface Transaction
+{
+
+    /**
+     * Commit already executed commands to the database.
+     * @returns boolean
+     */
+    public function commit();
+
+    /**
+     * Cancels the effect of previous commands and  "rolls back" the server.
+     */
+    public function cancel();
+
+}
