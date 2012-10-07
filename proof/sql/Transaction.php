164397594ddf2234377b39107869de12879f2738
developer <lmurray@trinistorm.org>
2012-10-07 00:13:33 -0400

    This class wraps around other implementors of the SQLTransaction interface. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ proof/sql/Transaction.php
@@ -0,0 +1,62 @@
+<?php
+namespace proof\sql;
+/**
+ * timestamp Oct 7, 2012 12:08:29 AM
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package proof\sql
+ *
+ *  Class representing transaction mode SQL.
+ *
+ */
+class Transaction implements SQLTransaction
+{
+
+    /**
+     * The SQLTransaction being wrapped.
+     * @var proof\sql\Transaction    $proxy
+     * @access private
+     */
+    private $proxy;
+
+    public function __construct(SQLTransaction $proxy)
+    {
+
+        $this->proxy = $proxy;
+
+    }
+
+    public function cancel()
+    {
+
+        $this->proxy->cancel();
+        return $this;
+    }
+
+    public function close()
+    {
+
+
+        $this->proxy->close();
+
+    }
+
+    public function commit()
+    {
+        return $this->proxy->commit();
+    }
+
+    public function create(String $sql)
+    {
+
+        return $this->proxy->create($sql);
+
+    }
+
+    public function prepare(String $sql)
+    {
+        return $this->proxy->prepare($sql);
+    }
+}
