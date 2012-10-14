bb8bd073e5c25d91124581eec172fcdf6d0d1462
developer <lmurray@trinistorm.org>
2012-10-14 09:34:47 -0400

    Provision for the Command pattern. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ proof/util/Command.php
@@ -0,0 +1,24 @@
+<?php
+namespace proof\util;
+/**
+ * timestamp Oct 13, 2012 9:34:43 PM
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package benson\main
+ *
+ * Interface for implementing the command pattern.
+ *
+ */
+interface Command
+{
+
+
+    /**
+     * When this method is called, the Command executes its workload.
+     * @return boolean | NULL
+     */
+    public function execute();
+
+}
