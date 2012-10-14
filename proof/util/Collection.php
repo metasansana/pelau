03ba03ce27de2d00abd377c27e0bcb151cd92d89
developer <lmurray@trinistorm.org>
2012-10-14 10:49:43 -0400

    Collection now has more responsibilities. Signed-off-by:developer <lmurray@trinistorm.org>

--- proof/util/Collection.php
+++ proof/util/Collection.php
@@ -3,22 +3,47 @@ namespace proof\util;
 /**
  * timestamp Jul 17, 2012 10:30:51 PM
  *
- * The Collection interface contains a set of methods that can be used to interact with a collection of data.
- * The interface itself does not have an add method, this was done to allow implementors to define
- * what types can be added to them via the add() signature.
- *
  * @author Lasana Murray  <dev@trinistorm.org>
  * @copyright 2012 Lasana Murray
  *
+ * The Colleciton interface provides methods for manipulating the data a class aggregates.
  *
  */
 interface Collection extends Aggregate
 {
 
     /**
-     * Adds an item to the Collection using the index of $index
+     * Returns the item stored at index if it exists.
+     * @param mixed $index    The index of the item desired.
+     * @return mixed|NULL    The item desired or NULL if none exists.
      */
-    public function add($index, $item);
+    public function get($index);
+
+    /**
+     * Checks if $index exists.
+     * @param mixed $index    The index of to find.
+     * @return boolean    TRUE if the index exists, FALSE otherwise.
+     */
+    public function indexAt($index);
+
+    /**
+     * Checks if an item exists at $index.
+     * @param mixed $index    The index of the item desired
+     * @return boolean    TRUE if an item at $index exists, FALSE otherwise.
+     */
+    public function itemAt($index);
+
+    /**
+     * Removes the item specified by $index.
+     * @param mixed $index    The index to remove.
+     * @return boolean    TRUE if successful, FALSE otherwise.
+     */
+    public function remove($index);
+
+    /**
+     * Removes all items.
+     */
+    public function clear();
 
 
 
