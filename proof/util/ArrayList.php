9e3472881ef067961a5fc76dc44931d511cdbd23
developer <lmurray@trinistorm.org>
2012-10-23 08:29:54 -0400

    ArrayList->remove() will recalculate keys internally. Signed-off-by:developer <lmurray@trinistorm.org>

--- proof/util/ArrayList.php
+++ proof/util/ArrayList.php
@@ -28,5 +28,21 @@ class ArrayList extends AbstractCollection
 
     }
 
+    /**
+     * Removes the item specified by $index. Note that the numeric keys are recalculated.
+     * @param mixed $index    The index to remove.
+     * @return boolean    TRUE if successful, FALSE otherwise.
+     */
+    public function remove($index)
+    {
+
+        $flag = parent::remove($index);
+
+        $this->items = array_values($this->items);
+
+        return $flag;
+
+    }
+
 
 }
