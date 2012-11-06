03824d2d9d4a9926298aef2efb78140edc5f86ee
developer <lmurray@trinistorm.org>
2012-11-06 04:50:51 -0400

    Added cookie support. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ proof/net/http/SetCookieException.php
@@ -0,0 +1,19 @@
+<?php
+namespace proof\net\http;
+/**
+ * timestamp Nov 6, 2012 4:45:20 AM
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package proof\net\http
+ *
+ * Thrown when unable to set a cookie.
+ *
+ */
+class SetCookieException extends \Exception
+{
+
+
+
+}
