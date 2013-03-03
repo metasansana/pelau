8b572a5e07308889c0aaa455f6b3b8b7b9fc55ec
developer <lmurray@trinistorm.org>
2013-03-03 11:05:02 -0400

    Changed interface to abstract class. Signed-off-by:developer <lmurray@trinistorm.org>

--- src/pelau/net/http/HttpListener.php
+++ src/pelau/net/http/HttpListener.php
@@ -1,22 +1,40 @@
 <?php
 namespace pelau\net\http;
 /**
- * timestamp Sep 9, 2012 6:57:39 PM
+ * timestamp Sep 9, 2012 6:59:57 PM
  *
  *
  * @author Lasana Murray  <dev@trinistorm.org>
  * @copyright 2012 Lasana Murray
  * @package pelau\net\http
  *
- *  Interface for listening to any HTTP event.
+ *  Abstract class for easier HttpListener implementation.
+ *
+ *  Abstract class for listening for HTTP events.
  *
  */
-interface HttpListener extends GetListener, PostListener
+abstract class HttpListener
 {
 
 
+    /**
+     * Method called when a GET event occurs.
+     * @param \pelau\net\http\Browser $client
+     * @param \pelau\net\http\GetEvent $evt
+     */
+    public function onGet(Browser $client, GetEvent $evt)
+    {
 
+    }
 
+    /**
+     * Method called when a POST event occurs.
+     * @param \pelau\net\http\Browser $client
+     * @param \pelau\net\http\PostEvent $evt
+     */
+    public function onPost(Browser $client, PostEvent $evt)
+    {
 
+    }
 
 }
