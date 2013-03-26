bbb2b623ec5e27ee1454f22e446f09577195ac0d
developer <lmurray@trinistorm.org>
2013-03-26 04:09:54 -0400

    Added generic connector for connecting to a database via PDO. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ src/pelau/sql/pdo/PDOConnector.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ *
+ * timestamp: Mar 25, 2013 8:59:09 PM
+ * encoding: UTF-8
+ *
+ * Copyright 2013  Lasana Murray <dev@trinistorm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package pelau\sql\pdo
+ */
+namespace pelau\sql\pdo;
+
+class PDOConnector
+{
+
+    /**
+     * Handles connection errors.
+     * @var \pelau\sql\pdo\PDOConnectionErrorHandler $errorHandler
+     * @access private
+     */
+    private $errorHandler;
+
+    public function __construct(PDOConnectionErrorHandler $errorHandler)
+    {
+
+        $this->errorHandler = $errorHandler;
+
+    }
+
+    public function connect($dsn, $user='', $passwd='')
+    {
+
+        try
+        {
+
+            $con =  new \pelau\sql\pdo\PDOConnection(new \PDO($dsn, $user, $passwd));
+
+            return $con;
+
+        }
+        catch (\PDOException $exc)
+        {
+
+            $this->errorHandler->onConnectionError($exc);
+
+        }
+
+
+    }
+
+}
