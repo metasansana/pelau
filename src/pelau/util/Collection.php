95d228ddcc100b5924b9da4834b9f25dbd927a1d
developer <lmurray@trinistorm.org>
2013-03-27 21:40:17 -0400

    Added size() method. Signed-off-by:developer <lmurray@trinistorm.org>

--- src/pelau/util/Collection.php
+++ src/pelau/util/Collection.php
@@ -57,6 +57,11 @@ abstract class Collection extends Aggregate implements \IteratorAggregate
 
     }
 
+    public function size()
+    {
+        return count($this->items);
+    }
+
     public function getIterator()
     {
 
