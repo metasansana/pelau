c9ee2fd45215ea107f355b5b5d542e921c3ba6fb
developer <lmurray@trinistorm.org>
2013-03-31 22:48:30 -0400

    Added simple classes for creating applications. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ src/pelau/app/web/AppLauncher.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ *
+ * timestamp: Mar 31, 2013 10:15:54 PM
+ * encoding: UTF-8
+ *
+ * Copyright 2013  Lasana Murray <dev@trinistorm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package pelau\app\web
+ */
+
+namespace pelau\app\web;
+
+use pelau\net\http\HttpListener;
+
+class AppLauncher
+{
+
+    /**
+     *
+     * @var \pelau\net\http\Browser
+     * @access private
+     */
+    private $brw;
+
+    /**
+     *
+     * @var array listeners
+     * @access private
+     */
+    private $listeners = array ();
+
+    public function __construct(\pelau\net\http\Browser $brw)
+    {
+
+        $this->brw = $brw;
+
+    }
+
+    public function onGet(HttpListener $l)
+    {
+
+        $this->listeners['GET'][] = function() use ($l) {
+
+                    $l->onGet($this->brw, new \pelau\net\http\GetEvent($this->brw));
+                };
+
+        return $this;
+
+    }
+
+    public function onPost(HttpListener $l)
+    {
+
+        $this->listeners['POST'][] = function() use ($l) {
+
+                    $l->onGet($this->brw, new \pelau\net\http\PostEvent($this->brw));
+                };
+
+        return $this;
+
+    }
+
+    public function launch($method)
+    {
+
+        $map = new \pelau\util\Map($this->listeners);
+
+        $list = $map->get($method);
+
+        if ($list)
+            foreach ($list as $f)
+                $f();
+
+    }
+
+}
---
+++ src/pelau/app/web/Application.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ *
+ * timestamp: Mar 31, 2013 10:42:54 PM
+ * encoding: UTF-8
+ *
+ * Copyright 2013  Lasana Murray <dev@trinistorm.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package pelau\app\web
+ *
+ * Abstract class for creating a pelau web app.
+ *
+ */
+namespace pelau\app\web;
+
+abstract class Application
+{
+
+    /**
+     * Constructs a new Application class. This method is final.
+     */
+    final public function __construct()
+    {
+
+         $this->main();
+
+    }
+
+    /**
+     * Child classes implement this method to indicate what classes will be created by the application.
+     * @var \pelau\app\web\AppLauncher $launcher
+     */
+    abstract protected function init(\pelau\app\web\AppLauncher $launcher);
+
+
+    /**
+     * Runs the Application
+     */
+    final private function run()
+    {
+
+        $launcher = new \pelau\app\web\AppLauncher(new \pelau\net\http\Browser);
+
+        $this->init($launcher);
+
+        $launcher->launch($_SERVER['REQUEST_METHOD']);
+
+    }
+
+
+
+}
